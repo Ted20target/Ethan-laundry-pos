b3c46f381462c67a563e02dbe4a5ac2fefb1c429
Teddy samwel <teddyotieno519@gmail.com>
2026-04-09 21:29:21 +0300

    Delete server directory

--- server/routes/reportRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getSummary } = require("../controllers/reportController");
-
-const router = express.Router();
-
-router.get("/summary", getSummary);
-
-module.exports = router;
